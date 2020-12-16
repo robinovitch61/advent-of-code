a2bdfbe7508736d549db9a33d889efff5a65e2a0
Leo Robinovitch <leorobinovitch@gmail.com>
2020-12-16 09:55:45 -0800

    add python template

---
+++ 2020/python-template.py
@@ -0,0 +1,30 @@
+
+with open('./16_input.txt', 'r') as f:
+    INPUT_TEXT = f.read()
+
+
+# with open('./16_test_input.txt', 'r') as f:
+#     TEST_INPUT_TEXT = f.read()
+
+
+def test_first():
+    pass
+
+
+def first():
+    pass
+
+
+def test_second():
+    pass
+
+
+def second():
+    pass
+
+
+if __name__ == "__main__":
+    test_first()
+    # first()
+    # test_second()
+    # second()
