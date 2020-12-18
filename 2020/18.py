ba831c113692fdac9e42306ecc045776633beaea
Leo Robinovitch <leorobinovitch@gmail.com>
2020-12-18 11:48:55 -0800

    day 18

--- 2020/18.py
+++ 2020/18.py
@@ -4,14 +4,25 @@ with open('./18_input.txt', 'r') as f:
 
 
 def eval_left_to_right(expr):
-    tokens = [v for v in expr.split(' ') if v != '']
+    tokens = expr.split(' ')
     while(len(tokens) > 1):
         first = tokens.pop(0)
         op = tokens.pop(0)
         second = tokens.pop(0)
         tokens = [eval(f"{first} {op} {second}")] + tokens
-    print(f"computed: {int(tokens[0])}")
     return int(tokens[0])
+
+
+def add_first(expr):
+    if ('+' not in expr) | ('*' not in expr):
+        return eval(expr)
+    else:
+        tokens = expr.split(' ')
+        idx = tokens.index('+')
+        first, second = tokens.pop(idx - 1), tokens.pop(idx)
+        tokens[idx - 1] = str(eval(f"{first} + {second}"))
+        new_expr = ' '.join(tokens)
+        return add_first(new_expr)
 
 
 def find_closing_paren(expr, start_idx):
@@ -26,72 +37,51 @@ def find_closing_paren(expr, start_idx):
     return start_idx
 
 
-def reduce_expr(expr):
+def evaluate(expr, precedence):
     if '(' not in expr:
-        return eval_left_to_right(expr)
-
-    # first_left_paren = expr.index('(')
-    close = find_closing_paren(expr, first_left_paren)
-    if first_left_paren == 0:
-        prefix, prefix_op = None, None
+        return precedence(expr)
     else:
-        prefix = expr[:first_left_paren - 3]
-        prefix_op = expr[first_left_paren - 2]
+        start = [idx for idx, val in enumerate(list(expr)) if val == '('][-1]  # last left paren in expr
+        end = find_closing_paren(expr, start)
+        new_expr = expr[:start] + str(precedence(expr[start + 1: end])) + expr[end + 1:]
+        return evaluate(new_expr, precedence)
 
-    in_parens = expr[first_left_paren + 1: close]
-
-    if close == len(expr) - 1:
-        postfix, postfix_op = None, None
-    else:
-        postfix = expr[close + 4:]
-        postfix_op = expr[close + 2]
-
-    print(f"expr: {expr}")
-    print(f"prefix: {prefix}")
-    print(f"prefix_op: {prefix_op}")
-    print(f"in_parens: {in_parens}")
-    print(f"postfix_op: {postfix_op}")
-    print(f"postfix: {postfix}")
-    final_expr = ''
-    if prefix:
-        final_expr += f"{reduce_expr(prefix)} {prefix_op} "
-    final_expr += f"{reduce_expr(in_parens)}"
-    if postfix:
-        final_expr += f" {postfix_op} {reduce_expr(postfix)}"
-
-    print(f"final_expr: {final_expr}")
-
-    return reduce_expr(final_expr)
-
-
-def evaluate(expr):
-    return reduce_expr(expr)
 
 
 def test_first():
-    # assert(evaluate("2 * 3 + (4 * 5)") == 26)
-    # assert(evaluate("5 + (8 * 3 + 9 + 3 * 4 * 3)") == 437)
-    # assert(evaluate("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))") == 12240)
-    assert(evaluate("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2") == 13632)
+    assert(evaluate("2 * 3 + (4 * 5)", eval_left_to_right) == 26)
+    assert(evaluate("5 + (8 * 3 + 9 + 3 * 4 * 3)", eval_left_to_right) == 437)
+    assert(evaluate("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", eval_left_to_right) == 12240)
+    assert(evaluate("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", eval_left_to_right) == 13632)
 
 
 def first():
     sum = 0
     for expr in INPUT_TEXT.split('\n'):
-        sum += evaluate(expr)
+        sum += evaluate(expr, eval_left_to_right)
     print(sum)
 
 
 def test_second():
-    pass
+    assert(add_first("1 + 2 * 3 + 4 * 5 + 6") == 231)
+    assert(add_first("1 + 6 + 44") == 51)
+    assert(add_first("2 * 3 + 20") == 46)
+    assert(evaluate("1 + (2 * 3) + (4 * (5 + 6))", add_first) == 51)
+    assert(evaluate("2 * 3 + (4 * 5)", add_first) == 46)
+    assert(evaluate("5 + (8 * 3 + 9 + 3 * 4 * 3)", add_first) == 1445)
+    assert(evaluate("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", add_first) == 669060)
+    assert(evaluate("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", add_first) == 23340)
 
 
 def second():
-    pass
+    sum = 0
+    for expr in INPUT_TEXT.split('\n'):
+        sum += evaluate(expr, add_first)
+    print(sum)
 
 
 if __name__ == "__main__":
     test_first()
-    # first()
-    # test_second()
-    # second()
+    first()
+    test_second()
+    second()
