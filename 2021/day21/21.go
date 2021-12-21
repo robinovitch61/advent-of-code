89988e1878012ae01564aceed4d479a2aadde6ec
Leo Robinovitch <leorobinovitch@gmail.com>
2021-12-21 14:49:23 -0800

    Day 21 part 2

--- 2021/day21/21.go
+++ 2021/day21/21.go
@@ -16,7 +16,7 @@ func p1(p1Start int, p2Start int) int {
 		p1Pos = newPosition(p1Pos, move)
 		p1Score += p1Pos
 		rolls += 3
-		if isGameOver(p1Score, target) {
+		if p1Score >= target {
 			break
 		}
 		move = updateMove(move)
@@ -24,82 +24,92 @@ func p1(p1Start int, p2Start int) int {
 		p2Pos = newPosition(p2Pos, move)
 		p2Score += p2Pos
 		rolls += 3
-		if isGameOver(p2Score, target) {
+		if p2Score >= target {
 			break
 		}
 		move = updateMove(move)
 	}
-	if isGameOver(p1Score, target) {
+	if p1Score >= target {
 		return p2Score * rolls
 	} else {
 		return p1Score * rolls
 	}
 }
 
-func getMoves() []int {
-	var moves []int
-	for i := 1; i <= 3; i++ {
-		for j := 1; j <= 3; j++ {
-			for k := 1; k <= 3; k++ {
-				moves = append(moves, i+j+k)
-			}
-		}
-	}
-	return moves
+type Player struct {
+	score, position int
 }
 
-func getNumWon(p1Pos, p2Pos, p1Score, p2Score, target int, p1Won, p2Won int64, p1Turn bool) (int64, int64) {
-	if isGameOver(p1Score, target) {
-		//fmt.Println("p1 won with", p1Score)
-		return p1Won + 1, p2Won
-	} else if isGameOver(p2Score, target) {
-		//fmt.Println("p2 won with", p2Score)
-		return p1Won, p2Won + 1
-	} else if p1Turn {
-		//fmt.Println("p1 turn with pos", p1Pos, "and score", p1Score)
-		for _, move := range getMoves() {
-			splitP1Pos := newPosition(p1Pos, move)
-			//fmt.Println("p1 rolled", move, "now at pos", splitP1Pos)
-			splitP1Score := p1Score + splitP1Pos
-			newP1Won, newP2Won := getNumWon(splitP1Pos, p2Pos, splitP1Score, p2Score, target, p1Won, p2Won, false)
-			p1Won += newP1Won - p1Won
-			p2Won += newP2Won - p2Won
-		}
-	} else {
-		//fmt.Println("p2 turn with pos", p2Pos, "and score", p2Score)
-		for _, move := range getMoves() {
-			splitP2Pos := newPosition(p2Pos, move)
-			splitP2Score := p2Score + splitP2Pos
-			newP1Won, newP2Won := getNumWon(p1Pos, splitP2Pos, p1Score, splitP2Score, target, p1Won, p2Won, true)
-			p1Won += newP1Won - p1Won
-			p2Won += newP2Won - p2Won
-		}
-	}
-	return p1Won, p2Won
+type GameState struct {
+	p1, p2 Player
 }
 
-func p2(p1Start int, p2Start int) int64 {
+type GameResult struct {
+	p1Won, p2Won int
+}
+
+type Memo map[GameState]GameResult
+
+func cacheResult(state GameState, result GameResult, memo Memo) GameResult {
+	memo[state] = result
+	return result
+}
+
+func getNumWon(state GameState, memo Memo, splitRollCounts map[int]int) GameResult {
+	if v, exists := memo[state]; exists {
+		return v
+	}
+	if state.p2.score >= 21 {
+		return cacheResult(state, GameResult{0, 1}, memo)
+	}
+	p1Won, p2Won := 0, 0
+	for move, count := range splitRollCounts {
+		newPos := newPosition(state.p1.position, move)
+		newScore := state.p1.score + newPos
+		result := getNumWon(GameState{state.p2, Player{newScore, newPos}}, memo, splitRollCounts)
+		p1Won += result.p2Won * count
+		p2Won += result.p1Won * count
+	}
+	return cacheResult(state, GameResult{p1Won, p2Won}, memo)
+}
+
+func p2(p1Start int, p2Start int) int {
 	defer common.Time()()
-	const target = 21
-	p1Won, p2Won := getNumWon(p1Start, p2Start, 0, 0, target, 0, 0, true)
-	fmt.Println(p1Won, p2Won)
-	if p1Won > p2Won {
-		return p1Won
+	memo := make(map[GameState]GameResult)
+	result := getNumWon(GameState{Player{0, p1Start}, Player{0, p2Start}}, memo, getSplitRollCounts())
+	fmt.Println(result)
+	if result.p1Won > result.p2Won {
+		return result.p1Won
 	} else {
-		return p2Won
+		return result.p2Won
 	}
 }
 
 func Run() {
 	common.PrintDay(21)
-	p1Start, p2Start := 4, 8
-	//p1Start, p2Start := 3, 4
+	//p1Start, p2Start := 4, 8
+	p1Start, p2Start := 3, 4
 	fmt.Println(p1(p1Start, p2Start))
 	fmt.Println(p2(p1Start, p2Start))
 }
 
-func isGameOver(score int, target int) bool {
-	return score >= target
+func getSplitRollCounts() map[int]int {
+	counts := make(map[int]int)
+	for i := 1; i <= 3; i++ {
+		for j := 1; j <= 3; j++ {
+			for k := 1; k <= 3; k++ {
+				sum := i + j + k
+				_, exists := counts[sum]
+				if exists {
+					counts[sum]++
+				} else {
+					counts[sum] = 1
+				}
+
+			}
+		}
+	}
+	return counts
 }
 
 func updateMove(move int) int {
