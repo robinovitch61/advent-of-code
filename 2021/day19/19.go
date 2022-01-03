939681f582ca949f327e245e3fabc61dac821ed2
Leo Robinovitch <leorobinovitch@gmail.com>
2022-01-03 15:37:05 -0700

    matMult

--- 2021/day19/19.go
+++ 2021/day19/19.go
@@ -24,6 +24,76 @@ type Scanner struct {
 	beacons []Beacon
 }
 
+type Matrix [][]int
+
+func (m Matrix) Equal(other Matrix) bool {
+	if len(m) != len(other) || len(m[0]) != len(other[0]) {
+		return false
+	}
+	for i := 0; i < len(m); i++ {
+		for j := 0; j < len(m[0]); j++ {
+			if m[i][j] != other[i][j] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func matMult(A, B Matrix) (Matrix, error) {
+	if len(A) == 0 || len(B) == 0 || len(A[0]) == 0 || len(B[0]) == 0 || len(A[0]) != len(B) {
+		return Matrix{}, fmt.Errorf("cannot multiply")
+	}
+	var res Matrix
+	for i := 0; i < len(A); i++ {
+		var row []int
+		for j := 0; j < len(B[0]); j++ {
+			entry := 0
+			for k := 0; k < len(A[0]); k++ {
+				entry += A[i][k] * B[k][j]
+			}
+			row = append(row, entry)
+		}
+		res = append(res, row)
+	}
+	return res, nil
+}
+
+func testMatMult() {
+	var A, B, expected Matrix
+	A = Matrix{
+		[]int{1, 2},
+		[]int{3, 4},
+	}
+	B = Matrix{
+		[]int{1},
+		[]int{2},
+	}
+	expected = Matrix{
+		// i k   k j   i k   k j
+		// 0,0 * 0,0 + 0,1 * 1,0
+		[]int{1*1 + 2*2},
+		// i k   k j   i k   k j
+		// 1,0 * 0,0 + 1,1 * 1,0
+		[]int{3*1 + 4*2},
+	}
+	if res, err := matMult(A, B); err != nil || !res.Equal(expected) {
+		fmt.Println(res)
+		fmt.Println(expected)
+		panic("")
+	}
+	B = A
+	expected = Matrix{
+		[]int{1*1 + 2*3, 1*2 + 2*4},
+		[]int{3*1 + 4*3, 3*2 + 4*4},
+	}
+	if res, err := matMult(A, B); err != nil || !res.Equal(expected) {
+		fmt.Println(res)
+		fmt.Println(expected)
+		panic("")
+	}
+}
+
 func parseInput(input []string) []Scanner {
 	text := strings.Join(input, "\n")
 	blocks := strings.Split(text, "\n\n")
@@ -44,7 +114,6 @@ func parseInput(input []string) []Scanner {
 
 func p1(scanners []Scanner) int {
 	defer common.Time()()
-	fmt.Println(scanners[0])
 	return -1
 }
 
@@ -53,8 +122,13 @@ func p2(scanners []Scanner) int {
 	return -1
 }
 
+func test() {
+	testMatMult()
+}
+
 func Run() {
 	common.PrintDay(19)
+	test()
 	input := common.ReadFile("19")
 	scanners := parseInput(input)
 	fmt.Println(p1(scanners))
