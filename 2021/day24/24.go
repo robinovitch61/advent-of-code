a92ac42e6efc8b7ef60c953fbfa6e67c42cdb14a
Leo Robinovitch <leorobinovitch@gmail.com>
2022-01-31 22:55:18 -0800

    Done

--- 2021/day24/24.go
+++ 2021/day24/24.go
@@ -3,126 +3,145 @@ package day24
 import (
 	"aoc/common"
 	"fmt"
-	"math"
-	"strconv"
-	"strings"
 )
 
-type Instruction struct {
-	kind, target string
-	valIsInt     bool
-	stringVal    string
-	intVal       int
-}
-
-type PuzzleInput []Instruction
-
-type Registers map[string]int
-
-func setVal(val string, instruction *Instruction) {
-	number, err := strconv.Atoi(val)
-	if err == nil {
-		instruction.intVal = number
-		instruction.valIsInt = true
-	} else {
-		instruction.stringVal = val
-	}
-}
-
-func parseInput(input []string) PuzzleInput {
-	var instructions []Instruction
-	for _, instrString := range input {
-		var instruction Instruction
-		split := strings.Split(instrString, " ")
-		instruction.kind = split[0]
-		instruction.target = split[1]
-		if len(split) == 3 {
-			setVal(split[2], &instruction)
+func getAllNumsDecreasing() []int {
+	var nums []int
+	for a := 9; a > 0; a-- {
+		for b := 9; b > 0; b-- {
+			for c := 9; c > 0; c-- {
+				for d := 9; d > 0; d-- {
+					for e := 9; e > 0; e-- {
+						for f := 9; f > 0; f-- {
+							for g := 9; g > 0; g-- {
+								combo := []int{a, b, c, d, e, f, g}
+								num := 0
+								tens := 1
+								for _, c := range combo {
+									num += c * tens
+									tens *= 10
+								}
+								nums = append(nums, num)
+							}
+						}
+					}
+				}
+			}
 		}
-		instructions = append(instructions, instruction)
 	}
-	return instructions
+	return nums
 }
 
-func getNext(stream *strings.Reader) int {
-	char, _ := stream.ReadByte()
-	num, _ := strconv.Atoi(string(char))
+func getAllNumsIncreasing() []int {
+	var nums []int
+	for a := 1; a < 10; a++ {
+		for b := 1; b < 10; b++ {
+			for c := 1; c < 10; c++ {
+				for d := 1; d < 10; d++ {
+					for e := 1; e < 10; e++ {
+						for f := 1; f < 10; f++ {
+							for g := 1; g < 10; g++ {
+								combo := []int{a, b, c, d, e, f, g}
+								num := 0
+								tens := 1
+								for _, c := range combo {
+									num += c * tens
+									tens *= 10
+								}
+								nums = append(nums, num)
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return nums
+}
+
+func split(num int) []int {
+	var nums []int
+	for num > 0 {
+		next := num % 10
+		nums = append([]int{next}, nums...)
+		num = num / 10
+	}
+	return nums
+}
+
+func join(nums []int) int {
+	num := 0
+	tens := 1
+	for i := 0; i < len(nums); i++ {
+		num += nums[i] * tens
+		tens *= 10
+	}
 	return num
 }
 
-func applyInstruction(stream *strings.Reader, reg Registers, instr Instruction) {
-	target := instr.target
-	var val int
-	if instr.kind != "inp" {
-		if instr.valIsInt {
-			val = instr.intVal
+func checkNum(num int, nums []Num) (int, bool) {
+	splitNum := split(num)
+	splitIdx := 0
+	z := 0
+	var aluInput []int
+
+	for _, n := range nums {
+		if n.isTwentySix {
+			next := (z % 26) + n.comp
+			if !(next > 0 && next < 10) {
+				return 0, false
+			}
+			aluInput = append([]int{next}, aluInput...)
+			z /= 26
 		} else {
-			val = reg[instr.stringVal]
+			next := splitNum[splitIdx]
+			aluInput = append([]int{next}, aluInput...)
+			z = z*26 + next + n.add
+			splitIdx++
 		}
 	}
-	switch instr.kind {
-	case "inp":
-		reg[target] = getNext(stream)
-	case "add":
-		reg[target] = reg[target] + val
-	case "mul":
-		reg[target] = reg[target] * val
-	case "div":
-		reg[target] = int(math.Floor(float64(reg[target] / val)))
-	case "mod":
-		reg[target] = reg[target] % val
-	case "eql":
-		if reg[target] == val {
-			reg[target] = 1
-		} else {
-			reg[target] = 0
-		}
-	}
+
+	return join(aluInput), true
 }
 
-func containsZero(num int) bool {
-	str := strconv.Itoa(num)
-	if strings.Contains(str, "0") {
-		return true
-	}
-	return false
-}
-
-func runProgram(in int, reg Registers, instructions []Instruction) Registers {
-	reg["w"], reg["x"], reg["y"], reg["z"] = 0, 0, 0, 0
-	stream := strings.NewReader(strconv.Itoa(in))
-	for _, instr := range instructions {
-		applyInstruction(stream, reg, instr)
-	}
-	return reg
-}
-
-func p1(puzzleInput PuzzleInput) int {
-	defer common.Time()()
-	reg := make(Registers)
-	for in := int(1e15 - 1); in >= 1e14; in-- {
-		if containsZero(in) {
-			continue
-		}
-		finalReg := runProgram(in, reg, puzzleInput)
-		if finalReg["z"] == 0 {
-			return in
-		}
-	}
-	return -1
-}
-
-func p2(puzzleInput PuzzleInput) int {
-	defer common.Time()()
-	return -1
+type Num struct {
+	isTwentySix bool
+	comp, add   int
 }
 
 func Run() {
 	common.PrintDay(24)
-	input := common.ReadFile("24")
-	puzzleInput := parseInput(input)
-	fmt.Println("99999999999999 =", runProgram(99999999999999, make(Registers), puzzleInput)["z"])
-	fmt.Println("11111111111111 =", runProgram(11111111111111, make(Registers), puzzleInput)["z"])
-	//fmt.Println(p1(puzzleInput))
-	//fmt.Println(p2(puzzleInput))
+	defer common.Time()()
+	nums := []Num{
+		{false, 11, 8},
+		{false, 12, 8},
+		{false, 10, 12},
+		{true, -8, 10},
+		{false, 15, 2},
+		{false, 15, 8},
+		{true, -11, 4},
+		{false, 10, 9},
+		{true, -3, 10},
+		{false, 15, 3},
+		{true, -3, 7},
+		{true, -1, 7},
+		{true, -10, 2},
+		{true, -16, 2},
+	}
+	// p1
+	allNums := getAllNumsDecreasing()
+	for _, num := range allNums {
+		if aluInput, isValid := checkNum(num, nums); isValid {
+			fmt.Println(aluInput)
+			break
+		}
+	}
+	// p2
+	allNums = getAllNumsIncreasing()
+	for _, num := range allNums {
+		if aluInput, isValid := checkNum(num, nums); isValid {
+			fmt.Println(aluInput)
+			break
+		}
+	}
 }
