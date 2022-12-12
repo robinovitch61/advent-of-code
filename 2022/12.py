087b59996c2d8718614604b078f528333a5b2f13
Leo Robinovitch <leorobinovitch@gmail.com>
2022-12-11 21:33:21 -0800

    Day 12 part 1

--- 2022/12.py
+++ 2022/12.py
@@ -1,4 +1,5 @@
 import string
+from collections import deque
 
 import common
 
@@ -6,25 +7,33 @@ PUZZLE = common.string(12)
 
 heights = {s: i for i, s in enumerate(string.ascii_lowercase)}
 heights.update({"S": 0})
+heights.update({"E": 25})
 
 
-def steps_to_finish(grid, visited, sx, sy):
-    visited[sy][sx] = True
+def get_adjacent(grid, sx, sy):
     adjacent = []
     w, h = len(grid[0]), len(grid)
     for dx, dy in ((-1, 0), (1, 0), (0, -1), (0, 1)):
         if 0 <= sx + dx < w and 0 <= sy + dy < h:
             adjacent.append((sx + dx, sy + dy))
+    return adjacent
 
-    to_finish = []
-    for ax, ay in adjacent:
-        if visited[ay][ax]:
-            continue
-        if grid[ay][ax] == "E":
-            return 1
-        if heights[grid[ay][ax]] - heights[grid[sy][sx]] <= 1:
-            to_finish.append(1 + steps_to_finish(grid, visited, ax, ay))
-    return min(to_finish) if len(to_finish) else 1e9
+
+def steps_to_finish(grid, sx, sy):
+    min_steps_to = [[float('inf') for _ in range(len(grid[0]))] for _ in range(len(grid))]
+    min_steps_to[sy][sx] = 0
+    q = deque()
+    q.append((sx, sy))
+    while len(q):
+        x, y = q.popleft()
+        curr_steps = min_steps_to[y][x]
+        for ax, ay in get_adjacent(grid, x, y):
+            if heights[grid[ay][ax]] - heights[grid[y][x]] <= 1:
+                if grid[ay][ax] == "E":
+                    return curr_steps + 1
+                if curr_steps + 1 < min_steps_to[ay][ax]:
+                    min_steps_to[ay][ax] = curr_steps + 1
+                    q.append((ax, ay))
 
 
 def first(puzzle):
@@ -33,8 +42,7 @@ def first(puzzle):
         for j in range(len(grid)):
             if grid[j][i] == "S":
                 sx, sy = i, j
-    visited = [[False for _ in range(len(grid[0]))] for _ in range(len(grid))]
-    return steps_to_finish(grid, visited, sx, sy)
+    return steps_to_finish(grid, sx, sy)
 
 
 def second(puzzle):
