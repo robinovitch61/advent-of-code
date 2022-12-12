c8d173010484e3b15334ae9915ce2f943cd677ca
Leo Robinovitch <leorobinovitch@gmail.com>
2022-12-11 22:13:01 -0800

    Minor cleanup day 12

--- 2022/12.py
+++ 2022/12.py
@@ -9,26 +9,25 @@ HEIGHTS = {s: i for i, s in enumerate(string.ascii_lowercase)}
 HEIGHTS.update({"S": 0, "E": 25})
 
 
-def get_adjacent(grid, sx, sy):
+def get_adjacent(grid, x, y):
     adjacent = []
     w, h = len(grid[0]), len(grid)
     for dx, dy in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-        if 0 <= sx + dx < w and 0 <= sy + dy < h:
-            adjacent.append((sx + dx, sy + dy))
+        if 0 <= x + dx < w and 0 <= y + dy < h:
+            adjacent.append((x + dx, y + dy))
     return adjacent
 
 
-def get_min_steps_to(grid, sx, sy, char):
+def get_min_steps_to(grid, sx, sy, seek):
     min_steps_to = [[float("inf") for _ in range(len(grid[0]))] for _ in range(len(grid))]
     min_steps_to[sy][sx] = 0
-    q = deque()
-    q.append((sx, sy))
+    q = deque(((sx, sy),))
     while len(q):
         x, y = q.popleft()
         curr_steps = min_steps_to[y][x]
         for ax, ay in get_adjacent(grid, x, y):
             if HEIGHTS[grid[y][x]] - HEIGHTS[grid[ay][ax]] <= 1:
-                if grid[ay][ax] == char:
+                if grid[ay][ax] == seek:
                     return curr_steps + 1
                 elif curr_steps + 1 < min_steps_to[ay][ax]:
                     min_steps_to[ay][ax] = curr_steps + 1
@@ -40,8 +39,7 @@ def solve(puzzle, char):
     for i in range(len(grid[0])):
         for j in range(len(grid)):
             if grid[j][i] == "E":
-                sx, sy = i, j
-    return get_min_steps_to(grid, sx, sy, char)
+                return get_min_steps_to(grid, i, j, char)
 
 
 def first(puzzle):
