6607920c6e45a86a9634fdb64cb2267ad8a4e352
Leo Robinovitch <leorobinovitch@gmail.com>
2022-12-11 22:04:50 -0800

    Wanna go fast

--- 2022/12.py
+++ 2022/12.py
@@ -5,9 +5,8 @@ import common
 
 PUZZLE = common.string(12)
 
-heights = {s: i for i, s in enumerate(string.ascii_lowercase)}
-heights.update({"S": 0})
-heights.update({"E": 25})
+HEIGHTS = {s: i for i, s in enumerate(string.ascii_lowercase)}
+HEIGHTS.update({"S": 0, "E": 25})
 
 
 def get_adjacent(grid, sx, sy):
@@ -19,8 +18,8 @@ def get_adjacent(grid, sx, sy):
     return adjacent
 
 
-def steps_to_finish(grid, sx, sy):
-    min_steps_to = [[float('inf') for _ in range(len(grid[0]))] for _ in range(len(grid))]
+def get_min_steps_to(grid, sx, sy, char):
+    min_steps_to = [[float("inf") for _ in range(len(grid[0]))] for _ in range(len(grid))]
     min_steps_to[sy][sx] = 0
     q = deque()
     q.append((sx, sy))
@@ -28,37 +27,37 @@ def steps_to_finish(grid, sx, sy):
         x, y = q.popleft()
         curr_steps = min_steps_to[y][x]
         for ax, ay in get_adjacent(grid, x, y):
-            if heights[grid[ay][ax]] - heights[grid[y][x]] <= 1:
-                if grid[ay][ax] == "E":
+            if HEIGHTS[grid[y][x]] - HEIGHTS[grid[ay][ax]] <= 1:
+                if grid[ay][ax] == char:
                     return curr_steps + 1
-                if curr_steps + 1 < min_steps_to[ay][ax]:
+                elif curr_steps + 1 < min_steps_to[ay][ax]:
                     min_steps_to[ay][ax] = curr_steps + 1
                     q.append((ax, ay))
 
 
-def first(puzzle):
+def solve(puzzle, char):
     grid = [list(l) for l in puzzle.split("\n")[:-1]]
     for i in range(len(grid[0])):
         for j in range(len(grid)):
-            if grid[j][i] == "S":
+            if grid[j][i] == "E":
                 sx, sy = i, j
-    return steps_to_finish(grid, sx, sy)
+    return get_min_steps_to(grid, sx, sy, char)
+
+
+def first(puzzle):
+    return solve(puzzle, "S")
 
 
 def second(puzzle):
-    grid = [list(l) for l in puzzle.split("\n")[:-1]]
-    all_a = []
-    for i in range(len(grid[0])):
-        for j in range(len(grid)):
-            if grid[j][i] == "a":
-                all_a.append((i, j))
-    return min(x for x in [steps_to_finish(grid, a[0], a[1]) for a in all_a] if x)
+    return solve(puzzle, "a")
 
 
 # `pytest *`
 def test():
     assert first(TEST_PUZZLE) == 31
-    assert second(TEST_PUZZLE) ==29
+    assert first(PUZZLE) == 472
+    assert second(TEST_PUZZLE) == 29
+    assert second(PUZZLE) == 465
 
 
 if __name__ == "__main__":
