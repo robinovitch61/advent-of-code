169ae397877fdcd9a826bca151e5123d2d67c901
Leo Robinovitch <leorobinovitch@gmail.com>
2022-12-12 23:54:29 -0800

    Use index

--- 2022/13.py
+++ 2022/13.py
@@ -8,21 +8,16 @@ PUZZLE = common.string(13)
 def compare(left, right):
     for l, r in zip(left, right):
         if isinstance(l, int) and isinstance(r, int):
-            if l < r:
-                return 1
-            elif l > r:
-                return -1
+            if l != r:
+                return 1 if l < r else -1
         elif isinstance(l, list) and isinstance(r, list):
-            res = compare(l, r)
-            if res is not None:
+            if (res := compare(l, r)) is not None:
                 return res
         elif isinstance(l, list) and isinstance(r, int):
-            res = compare(l, [r])
-            if res is not None:
+            if (res := compare(l, [r])) is not None:
                 return res
         elif isinstance(l, int) and isinstance(r, list):
-            res = compare([l], r)
-            if res is not None:
+            if (res := compare([l], r)) is not None:
                 return res
     if len(left) != len(right):
         return 1 if len(left) < len(right) else -1
@@ -32,36 +27,27 @@ def first(puzzle):
     res = 0
     for i, pair in enumerate(puzzle.split("\n\n")):
         split = pair.split("\n")
-        left, right = eval(split[0]), eval(split[1])
-        comp = compare(left, right)
+        comp = compare(eval(split[0]), eval(split[1]))
         if comp == 1:
             res += i + 1
     return res
 
 
 def second(puzzle):
-    packets = []
+    packets = [[[2]], [[6]]]
     for l in puzzle.split("\n"):
         if len(l):
             packets.append(eval(l))
-    packets.append([[2]])
-    packets.append([[6]])
-    packets.sort(key=functools.cmp_to_key(compare))
-    packets = reversed(packets)
-    res = 1
-    for i, p in enumerate(packets):
-        if p == [[2]] or p == [[6]]:
-            res *= (i + 1)
-    return res
+    sorted_packets = list(reversed(sorted(packets, key=functools.cmp_to_key(compare))))
+    return (sorted_packets.index([[2]]) + 1) * (sorted_packets.index([[6]]) + 1)
 
 
 # `pytest *`
 def test():
     assert first(TEST_PUZZLE) == 13
-    assert first(PUZZLE) > 4390
-    assert first(PUZZLE) < 5396
-    assert first(PUZZLE) > 5217
+    assert first(PUZZLE) == 5366
     assert second(TEST_PUZZLE) == 140
+    assert second(PUZZLE) == 23391
 
 
 if __name__ == "__main__":
