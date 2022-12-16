e5ec8cbb8cb324be488c82781cb81dfb2245f2b9
Leo Robinovitch <leorobinovitch@gmail.com>
2022-12-16 14:42:27 -0800

    Wrong?

--- 2022/16.py
+++ 2022/16.py
@@ -17,55 +17,37 @@ Valve II has flow rate=0; tunnels lead to valves AA, JJ
 Valve JJ has flow rate=21; tunnel leads to valve II
 """
 
-puzzle = TEST_PUZZLE
-graph = {}
-flow_rates = {}
-for line in puzzle.split("\n")[:-1]:
-    valve, flow_rate, conns = \
-        re.findall(r"Valve ([A-Z]{2}) has flow rate=(\d+); tunnels? leads? to valves? (.*)", line)[0]
-    graph[valve] = conns.split(", ")
-    flow_rates[valve] = int(flow_rate)
-
-
 cache = {}
 
 
-def max_pressure_released(current_valve, open_valves, time, path):
-    val, cached_path = cache.get((current_valve, open_valves, time), (None, None))
-    if (val, cached_path) != (None, None):
-        return val, cached_path
-    current_pressure_release = sum(flow_rates[v] for v in open_valves)
-    if time >= 30:
-        return 0, path
+def max_pressure_released(flow_rates, graph, current_valve, open_valves, time):
+    if (res := cache.get((current_valve, open_valves, time))) is not None:
+        return res
+    if time > 30:
+        return 0
+    release = sum(flow_rates[v] for v in open_valves)
     if flow_rates[current_valve] > 0 and current_valve not in open_valves:
-        current_pressure_release *= 2
         open_valves = open_valves + (current_valve,)
-        time += 1
-        path = path + (f"{time} ({current_valve}: open {current_valve}",)
+        res = release + max_pressure_released(flow_rates, graph, current_valve, open_valves, time + 1)
+        cache[(current_valve, open_valves, time)] = res
+        return res
     max_pressure_release = 0
-    next_path = None
     for next_valve in graph[current_valve]:
-        max_next, path = max_pressure_released(next_valve, open_valves, time + 1, path)
-        if (current_pressure_release + max_next) > max_pressure_release:
-            max_pressure_release = current_pressure_release + max_next
-            next_path = next_valve
-    path = path + (f"{time} ({current_valve}): move to {next_path}",)
-    cache[(current_valve, open_valves, time)] = max_pressure_release, path
-    return max_pressure_release, path
+        max_next = release + max_pressure_released(flow_rates, graph, next_valve, open_valves, time + 1)
+        max_pressure_release = max(max_pressure_release, max_next)
+    cache[(current_valve, open_valves, time)] = max_pressure_release
+    return max_pressure_release
 
 
 def first(puzzle):
-    # puzzle = TEST_PUZZLE
-    # graph = {}
-    # flow_rates = {}
-    # for line in puzzle.split("\n")[:-1]:
-    #     valve, flow_rate, conns = \
-    #         re.findall(r"Valve ([A-Z]{2}) has flow rate=(\d+); tunnels? leads? to valves? (.*)", line)[0]
-    #     graph[valve] = conns.split(", ")
-    #     flow_rates[valve] = int(flow_rate)
-    res, path = max_pressure_released("AA", tuple(), 1, tuple())
-    print(path)
-    return res
+    graph = {}
+    flow_rates = {}
+    for line in puzzle.split("\n")[:-1]:
+        valve, flow_rate, conns = \
+            re.findall(r"Valve ([A-Z]{2}) has flow rate=(\d+); tunnels? leads? to valves? (.*)", line)[0]
+        graph[valve] = conns.split(", ")
+        flow_rates[valve] = int(flow_rate)
+    return max_pressure_released(flow_rates, graph, "AA", tuple(), 1)
 
 
 def second(puzzle):
