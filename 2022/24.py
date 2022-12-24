1bad98349489bd35aef52a87a8ea32d151c94184
Leo Robinovitch <leorobinovitch@gmail.com>
2022-12-24 14:17:43 -0800

    Complete Day 24

--- 2022/24.py
+++ 2022/24.py
@@ -63,7 +63,8 @@ def update_blizzards(w, h, blizzards):
     return new_blizzards
 
 
-def smallest_mins_to_end(mins, i, j, w, h, blizzards):
+def smallest_mins_to_end(i, j, w, h, end_i, end_j, blizzards):
+    mins = 0
     positions = {(i, j)}
 
     while True:
@@ -73,37 +74,50 @@ def smallest_mins_to_end(mins, i, j, w, h, blizzards):
 
         for i, j in positions:
             # done
-            if i == w - 1 and j == h - 1:
-                return mins
+            if abs(i - end_i) <= 1 and abs(j - end_j) <= 1:
+                if abs(i - end_i) == 0 or abs(j - end_j) == 0:
+                    return mins, blizzards
 
             # wait
             if (i, j) not in blizzards:
                 new_positions.add((i, j))
 
             # move
-            if i is None:
-                if (0, 0) not in blizzards:
-                    new_positions.add((0, 0))
-            else:
-                for a in adjacent(w, h, i, j):
-                    if a not in blizzards:
-                        new_positions.add(a)
+            for a in adjacent(w, h, i, j):
+                if a not in blizzards:
+                    new_positions.add(a)
 
         positions = new_positions
 
 
 def first(puzzle):
     w, h, blizzards = parse(puzzle)
-    return smallest_mins_to_end(0, None, None, w, h, blizzards)
+    mins, _ = smallest_mins_to_end(0, -1, w, h, w - 1, h, blizzards)
+    return mins
 
 
 def second(puzzle):
-    return -1
+    res = 0
+    w, h, blizzards = parse(puzzle)
+
+    # there
+    mins, blizzards = smallest_mins_to_end(0, -1, w, h, w - 1, h, blizzards)
+    res += mins
+
+    # back
+    mins, blizzards = smallest_mins_to_end(w - 1, h, w, h, 0, -1, blizzards)
+    res += mins
+
+    # there again
+    mins, blizzards = smallest_mins_to_end(0, -1, w, h, w - 1, h, blizzards)
+    return res + mins
 
 
 def test():
     assert first(TEST_PUZZLE) == 18
-    assert second(TEST_PUZZLE) == -1
+    assert first(PUZZLE) == 262
+    assert second(TEST_PUZZLE) == 54
+    assert second(PUZZLE) == 785
 
 
 if __name__ == "__main__":
