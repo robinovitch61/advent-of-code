aea4ccf47919b1d433681afcac9008277e957d46
Leo Robinovitch <leorobinovitch@gmail.com>
2022-12-26 13:27:19 -0800

    Day 20 Part 1

--- 2022/20.py
+++ 2022/20.py
@@ -4,81 +4,30 @@ PUZZLE = common.lines(20)
 
 
 def first(puzzle):
-    # start
-    # idxs: 0  1   2  3   4  5  6
-    # id:   0  1   2  3   4  5  6
-    # nums: 1, 2, -3, 3, -2, 0, 4
-
-    # 1
-    # idxs: 0  1   2  3   4  5  6
-    # id:   1  0   2  3   4  5  6
-    # nums: 2, 1, -3, 3, -2, 0, 4
-
-    # 2
-    # idxs: 0  1   2  3   4  5  6
-    # id:   0  1,  2  3   4  5  6
-    # nums: 1, 2, -3, 3, -2, 0, 4
-
-    # idxs: 0   1  2  3   4  5  6
-    # id:   0   2  1  3   4  5  6
-    # nums: 1, -3, 2, 3, -2, 0, 4
-
-    # -3
-    # idxs:  0  1  2  3   4  5  6
-    # id:    2  0  1  3   4  5  6  ##
-    # nums: -3, 1, 2, 3, -2, 0, 4
-
-    # idxs: 0  1  2   3  4   5  6
-    # id:   0  1  3   4  5   2  6  ##
-    # nums: 1, 2, 3, -2, 0, -3, 4
-
-    # idxs: 0  1  2   3   4  5  6
-    # id:   0  1  3   4   2  5  6
-    # nums: 1, 2, 3, -2, -3, 0, 4
-
     nums = list(map(int, puzzle))
-    id_to_num = {i: n for i, n in enumerate(nums)}  # static
-    id_to_idx = {i: i for i, _ in enumerate(nums)}
-    idx_to_id = {i: i for i, _ in enumerate(nums)}
-    for id, num in list(id_to_num.items())[:4]:
-        print(id, num)
-        left = num < 0
-        for move in range(abs(num)):
-            print(move+1)
-            if left:
-                if id_to_idx[id] == 0:
-                    for left_idx in range(len(nums) - 2):
-                        right_idx = left_idx + 1
-                        idx_to_id[left_idx], id_to_idx[right_idx] = idx_to_id[right_idx], id_to_idx[left_idx]
-                    place_idx = len(nums) - 2
-                    id_to_idx[id] = place_idx
-                    idx_to_id[place_idx] = id
-                else:
-                    right_idx = id_to_idx[id]
-                    left_idx = right_idx - 1
-                    right_id = id
-                    left_id = idx_to_id[left_idx]
-                    id_to_idx[right_id], id_to_idx[left_id] = id_to_idx[left_id], id_to_idx[right_id]
-                    idx_to_id[right_idx], idx_to_id[left_idx] = idx_to_id[left_idx], idx_to_id[right_idx]
+    unique_nums = [i for i, _ in enumerate(nums)]
+    unique_num_to_num = {u: n for u, n in zip(unique_nums, nums)}
+    for un in range(len(unique_nums)):
+        curr_idx = unique_nums.index(un)
+        amount_to_move = unique_num_to_num[un]
+        if amount_to_move > 0:
+            if curr_idx + amount_to_move < len(unique_nums):
+                num = unique_nums.pop(curr_idx)
+                unique_nums.insert((curr_idx + amount_to_move) % len(unique_nums), num)
             else:
-                if id_to_idx[id] == len(nums) - 1:
-                    # TODO: handle wrap
-                    pass
-                else:
-                    print(id_to_idx)
-                    print([f"{idx:2}" for idx in range(len(nums))])
-                    print([f"{idx_to_id[idx]:2}" for idx in range(len(nums))])
-                    print([f"{id_to_num[idx_to_id[idx]]:2}" for idx in range(len(nums))])
-                    print("")
-                    left_idx = id_to_idx[id]
-                    right_idx = left_idx + 1
-                    left_id = id
-                    right_id = idx_to_id[right_idx]
-                    print(left_idx, right_idx, left_id, right_id)
-                    id_to_idx[right_id], id_to_idx[left_id] = id_to_idx[left_id], id_to_idx[right_id]
-                    idx_to_id[right_idx], idx_to_id[left_idx] = idx_to_id[left_idx], idx_to_id[right_idx]
-
-    return -1
+                wrap = (curr_idx + amount_to_move) - len(unique_nums) + 1
+                num = unique_nums.pop(curr_idx)
+                unique_nums.insert(wrap % len(unique_nums), num)
+        else:
+            if curr_idx + amount_to_move > 0:
+                num = unique_nums.pop(curr_idx)
+                unique_nums.insert((curr_idx + amount_to_move) % len(unique_nums), num)
+            else:
+                wrap = len(unique_nums) + (curr_idx + amount_to_move) - 1
+                num = unique_nums.pop(curr_idx)
+                unique_nums.insert(wrap % len(unique_nums), num)
+    shuffled = [unique_num_to_num[un] for un in unique_nums]
+    return sum(shuffled[(shuffled.index(0) + idx) % len(shuffled)] for idx in (1000, 2000, 3000))
 
 
 def second(puzzle):
@@ -86,7 +35,8 @@ def second(puzzle):
 
 
 def test():
-    assert first(TEST_PUZZLE) == -1
+    assert first(TEST_PUZZLE) == 3
+    assert first(PUZZLE) != -2910
     assert second(TEST_PUZZLE) == -1
 
 
