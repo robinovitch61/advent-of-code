43b21385393dc686396fbb8e61853afd9974480a
Leo Robinovitch <leorobinovitch@gmail.com>
2022-12-26 13:45:19 -0800

    Day 20 Part 2

--- 2022/20.py
+++ 2022/20.py
@@ -3,10 +3,7 @@ import common
 PUZZLE = common.lines(20)
 
 
-def first(puzzle):
-    nums = list(map(int, puzzle))
-    unique_nums = [i for i, _ in enumerate(nums)]
-    unique_num_to_num = {u: n for u, n in zip(unique_nums, nums)}
+def mix(unique_nums, unique_num_to_num):
     for un in range(len(unique_nums)):
         curr_idx = unique_nums.index(un)
         amount_to_move = unique_num_to_num[un]
@@ -26,18 +23,34 @@ def first(puzzle):
                 wrap = len(unique_nums) + (curr_idx + amount_to_move) - 1
                 num = unique_nums.pop(curr_idx)
                 unique_nums.insert(wrap % len(unique_nums), num)
-    shuffled = [unique_num_to_num[un] for un in unique_nums]
-    return sum(shuffled[(shuffled.index(0) + idx) % len(shuffled)] for idx in (1000, 2000, 3000))
+    return unique_nums
+
+
+def first(puzzle):
+    nums = list(map(int, puzzle))
+    unique_nums = [i for i, _ in enumerate(nums)]
+    unique_num_to_num = {u: n for u, n in zip(unique_nums, nums)}
+    mixed_unique_nums = mix(unique_nums, unique_num_to_num)
+    mixed_nums = [unique_num_to_num[n] for n in mixed_unique_nums]
+    return sum(mixed_nums[(mixed_nums.index(0) + idx) % len(mixed_nums)] for idx in (1000, 2000, 3000))
 
 
 def second(puzzle):
-    return -1
+    nums = list(map(int, puzzle))
+    nums = [n * 811589153 for n in nums]
+    unique_nums = [i for i, _ in enumerate(nums)]
+    unique_num_to_num = {u: n for u, n in zip(unique_nums, nums)}
+    mixed = unique_nums
+    for _ in range(10):
+        mixed = mix(mixed, unique_num_to_num)
+    mixed_nums = [unique_num_to_num[n] for n in mixed]
+    return sum(mixed_nums[(mixed_nums.index(0) + idx) % len(mixed_nums)] for idx in (1000, 2000, 3000))
 
 
 def test():
     assert first(TEST_PUZZLE) == 3
     assert first(PUZZLE) != -2910
-    assert second(TEST_PUZZLE) == -1
+    assert second(TEST_PUZZLE) == 1623178306
 
 
 if __name__ == "__main__":
