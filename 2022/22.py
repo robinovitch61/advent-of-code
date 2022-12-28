bfd80023682ceaa1041a887445f9e9b9fe1659ce
Leo Robinovitch <leorobinovitch@gmail.com>
2022-12-27 19:20:58 -0800

    Day 22 Part 2

--- 2022/22.py
+++ 2022/22.py
@@ -30,6 +30,42 @@ CLOCKWISE = {
     "^": ">",
 }
 COUNTER = {v: k for k, v in CLOCKWISE.items()}
+UP, DOWN, LEFT, RIGHT = "^", "v", "<", ">"
+
+# These are manually defined based on some paper cubes I made
+# (current_face, direction): (target_face, new_direction, flip)
+TEST_TELEPORT_MAP = {
+    (1, UP): (2, DOWN, True),
+    (1, LEFT): (3, DOWN, False),
+    (1, RIGHT): (6, LEFT, True),
+    (2, UP): (1, DOWN, True),
+    (2, DOWN): (5, UP, True),
+    (2, LEFT): (6, UP, True),
+    (3, UP): (1, RIGHT, False),
+    (3, DOWN): (5, RIGHT, True),
+    (4, RIGHT): (6, DOWN, True),
+    (5, DOWN): (2, UP, True),
+    (5, LEFT): (3, UP, True),
+    (6, UP): (4, LEFT, True),
+    (6, DOWN): (2, RIGHT, True),
+    (6, RIGHT): (1, LEFT, True),
+}
+TELEPORT_MAP = {
+    (1, UP): (6, RIGHT, False),
+    (1, LEFT): (4, RIGHT, True),
+    (2, UP): (6, UP, False),
+    (2, DOWN): (3, LEFT, False),
+    (2, RIGHT): (5, LEFT, True),
+    (3, LEFT): (4, DOWN, False),
+    (3, RIGHT): (2, UP, False),
+    (4, UP): (3, RIGHT, False),
+    (4, LEFT): (1, RIGHT, True),
+    (5, DOWN): (6, LEFT, False),
+    (5, RIGHT): (2, LEFT, True),
+    (6, DOWN): (2, DOWN, False),
+    (6, LEFT): (1, DOWN, False),
+    (6, RIGHT): (5, UP, False),
+}
 
 
 def parse(puzzle):
@@ -48,7 +84,7 @@ def parse(puzzle):
 
 
 def move(board, r, c, d):
-    if d == ">":
+    if d == RIGHT:
         if board.get((r, c + 1)) == OPEN:
             return r, c + 1
         elif board.get((r, c + 1)) == WALL:
@@ -59,7 +95,7 @@ def move(board, r, c, d):
                 return r, c
             else:
                 return r, cc
-    elif d == "<":
+    elif d == LEFT:
         if board.get((r, c - 1)) == OPEN:
             return r, c - 1
         elif board.get((r, c - 1)) == WALL:
@@ -70,7 +106,7 @@ def move(board, r, c, d):
                 return r, c
             else:
                 return r, cc
-    elif d == "^":
+    elif d == UP:
         if board.get((r - 1, c)) == OPEN:
             return r - 1, c
         if board.get((r - 1, c)) == WALL:
@@ -81,7 +117,7 @@ def move(board, r, c, d):
                 return r, c
             else:
                 return rr, c
-    elif d == "v":
+    elif d == DOWN:
         if board.get((r + 1, c)) == OPEN:
             return r + 1, c
         if board.get((r + 1, c)) == WALL:
@@ -99,7 +135,7 @@ def move(board, r, c, d):
 def first(puzzle):
     board, commands = parse(puzzle)
     r, c = min(board.keys())  # leftmost open tile
-    d = ">"
+    d = RIGHT
     for command in commands:
         if command == "L":
             d = COUNTER[d]
@@ -114,12 +150,13 @@ def first(puzzle):
     return 1000 * (r + 1) + 4 * (c + 1) + list(CLOCKWISE.keys()).index(d)
 
 
-def get_pos_to_face(board):
+def get_face_info(board):
     rs, cs = zip(*board.keys())
     height, width = max(rs) - min(rs), max(cs) - min(cs)
     side = (min(height, width) + 1) // 3
     face = 0
     pos_to_face = {}
+    face_to_top_left_pos = {}
     down = 0
     across = 0
     while True:
@@ -128,9 +165,10 @@ def get_pos_to_face(board):
             if across > side * 4:
                 down += side
                 if down > side * 4:
-                    return pos_to_face
+                    return side, pos_to_face, face_to_top_left_pos
                 across = 0
         face += 1
+        face_to_top_left_pos[face] = (down, across)
         for i in range(side):
             for j in range(side):
                 assert (down + i, across + j) in board
@@ -138,19 +176,107 @@ def get_pos_to_face(board):
         across += side
 
 
-def second(puzzle):
+def move_cube(board, teleport_func, r, c, d):
+    if d == RIGHT:
+        if board.get((r, c + 1)) == OPEN:
+            return r, c + 1, d
+        elif board.get((r, c + 1)) == WALL:
+            return r, c, d
+        else:
+            return teleport_func(r, c, d)
+    elif d == LEFT:
+        if board.get((r, c - 1)) == OPEN:
+            return r, c - 1, d
+        elif board.get((r, c - 1)) == WALL:
+            return r, c, d
+        else:
+            return teleport_func(r, c, d)
+    elif d == UP:
+        if board.get((r - 1, c)) == OPEN:
+            return r - 1, c, d
+        if board.get((r - 1, c)) == WALL:
+            return r, c, d
+        else:
+            return teleport_func(r, c, d)
+    elif d == DOWN:
+        if board.get((r + 1, c)) == OPEN:
+            return r + 1, c, d
+        if board.get((r + 1, c)) == WALL:
+            return r, c, d
+        else:
+            return teleport_func(r, c, d)
+    else:
+        raise Exception(d)
+
+
+def make_teleport_func(board, side, pos_to_face, face_to_top_left_pos, teleport_map):
+    """Returns a function that maps an edge position (and direction?) to the next position and direction."""
+
+    def func(r, c, d):
+        curr_face = pos_to_face[(r, c)]
+        next_face, next_direction, flip = teleport_map[(curr_face, d)]
+        curr_down, curr_across = face_to_top_left_pos[curr_face]
+        next_down, next_across = face_to_top_left_pos[next_face]
+        if d in (LEFT, RIGHT):
+            offset = r - curr_down
+        elif d in (UP, DOWN):
+            offset = c - curr_across
+        else:
+            raise Exception(d)
+        if flip:
+            offset = side - 1 - offset
+        # assert offset > 0
+
+        if next_direction == UP:
+            rr = next_down + side - 1
+            cc = next_across + offset
+        elif next_direction == DOWN:
+            rr = next_down
+            cc = next_across + offset
+        elif next_direction == LEFT:
+            rr = next_down + offset
+            cc = next_across + side - 1
+        elif next_direction == RIGHT:
+            rr = next_down + offset
+            cc = next_across
+        else:
+            raise Exception(next_direction)
+
+        if board[(rr, cc)] == WALL:
+            return r, c, d
+
+        return rr, cc, next_direction
+
+    return func
+
+
+def second(puzzle, teleport_map):
     board, commands = parse(puzzle)
-    pos_to_face = get_pos_to_face(board)
-    return -1
+    side, pos_to_face, face_to_top_left_pos = get_face_info(board)
+    teleport_func = make_teleport_func(board, side, pos_to_face, face_to_top_left_pos, teleport_map)
+    r, c = min(board.keys())  # leftmost open tile
+    d = RIGHT
+    for command in commands:
+        if command == "L":
+            d = COUNTER[d]
+        elif command == "R":
+            d = CLOCKWISE[d]
+        else:
+            for _ in range(command):
+                new_r, new_c, new_d = move_cube(board, teleport_func, r, c, d)
+                if new_r == r and new_c == c:
+                    break
+                r, c, d = new_r, new_c, new_d
+    return 1000 * (r + 1) + 4 * (c + 1) + list(CLOCKWISE.keys()).index(d)
 
 
 def test():
     assert first(TEST_PUZZLE) == 6032
     assert first(PUZZLE) == 126350
-    assert second(TEST_PUZZLE) == -1
-    assert second(PUZZLE) == -1
+    assert second(TEST_PUZZLE, TEST_TELEPORT_MAP) == 5031
+    # assert second(TEST_PUZZLE, TELEPORT_MAP) == 5031
 
 
 if __name__ == "__main__":
     print(first(PUZZLE))
-    print(second(PUZZLE))
+    print(second(PUZZLE, TELEPORT_MAP))
