ec094c2d40853d82e711d6578881828e889b7641
Leo Robinovitch <leorobinovitch@gmail.com>
2022-12-29 12:53:18 -0800

    Day 19 updates

--- 2022/19.py
+++ 2022/19.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+import functools
 import re
 from collections import deque
 from copy import copy
@@ -29,8 +32,9 @@ class Blueprint:
     obs_for_geode_robot: int
 
 
-@dataclass
+@functools.total_ordering
 class Inventory:
+    bp: Blueprint
     ore: int = 0
     ore_robots: int = 1
     pending_ore_robots: Tuple[int] = ()
@@ -44,38 +48,57 @@ class Inventory:
     geode_robots: int = 0
     pending_geode_robots: Tuple[int] = ()
 
+    def __init__(self, bp):
+        self.bp = bp
 
-def potential_inventories(i: Inventory, bp: Blueprint) -> List[Inventory]:
+    def _score(self):
+        # if have ore and obsidian, lose score for how far away you are from buying geode robot
+        return self.geodes
+        # return (
+        #     (self.geodes + self.geode_robots + len(self.pending_geode_robots))
+        #     + (self.obs + self.obs_robots + len(self.pending_obs_robots)) / 2
+        #     + (self.clay + self.clay_robots + len(self.pending_clay_robots)) / 3
+        #     + (self.ore + self.ore_robots + len(self.pending_ore_robots)) / 4
+        # )
+
+    def __lt__(self, other: Inventory):
+        return self._score() < other._score()
+
+    def __eq__(self, other: Inventory):
+        return self._score() == other._score()
+
+
+def potential_inventories(i: Inventory) -> List[Inventory]:
     inventories = [i]
 
     # can buy geode robot
-    if i.ore >= bp.ore_for_geode_robot and i.obs >= bp.obs_for_geode_robot:
+    if i.ore >= i.bp.ore_for_geode_robot and i.obs >= i.bp.obs_for_geode_robot:
         ni = copy(i)
-        ni.ore -= bp.ore_for_geode_robot
-        ni.obs -= bp.obs_for_geode_robot
-        ni.pending_geode_robots += (2,)
+        ni.ore -= i.bp.ore_for_geode_robot
+        ni.obs -= i.bp.obs_for_geode_robot
+        ni.pending_geode_robots += (1,)
         inventories.append(ni)
 
-        # can buy obs robot
-    if i.ore >= bp.ore_for_obs_robot and i.clay >= bp.clay_for_obs_robot:
+    # can buy obs robot
+    if i.ore >= i.bp.ore_for_obs_robot and i.clay >= i.bp.clay_for_obs_robot:
         ni = copy(i)
-        ni.ore -= bp.ore_for_obs_robot
-        ni.clay -= bp.clay_for_obs_robot
-        ni.pending_obs_robots += (2,)
+        ni.ore -= i.bp.ore_for_obs_robot
+        ni.clay -= i.bp.clay_for_obs_robot
+        ni.pending_obs_robots += (1,)
         inventories.append(ni)
 
-        # can buy clay robot
-    if i.ore >= bp.ore_for_clay_robot:
+    # can buy clay robot
+    if i.ore >= i.bp.ore_for_clay_robot:
         ni = copy(i)
-        ni.ore -= bp.ore_for_clay_robot
-        ni.pending_clay_robots += (2,)
+        ni.ore -= i.bp.ore_for_clay_robot
+        ni.pending_clay_robots += (1,)
         inventories.append(ni)
 
-        # can buy ore robot
-    if i.ore >= bp.ore_for_ore_robot:
+    # can buy ore robot
+    if i.ore >= i.bp.ore_for_ore_robot:
         ni = copy(i)
-        ni.ore -= bp.ore_for_ore_robot
-        ni.pending_ore_robots += (2,)
+        ni.ore -= i.bp.ore_for_ore_robot
+        ni.pending_ore_robots += (1,)
         inventories.append(ni)
 
     return inventories
@@ -128,15 +151,23 @@ def get_blueprints(puzzle):
 
 
 def quality_level(bp: Blueprint):
-    inventories = deque([Inventory()])
+    inventories = deque([Inventory(bp=bp)])
+    max_inventories = 100000
+    # kill_no_clay_robot_inventories_starting_at = bp.ore_for_clay_robot + 1
+    # kill_no_geode_inventories_starting_at = 20
     for t in range(24):
+        print(t, len(inventories))
         for i in range(len(inventories)):
-            for ni in potential_inventories(inventories.popleft(), bp):
+            for ni in potential_inventories(inventories.popleft()):
                 inventories.append(ni)
         inventories = deque([update(i) for i in inventories])
-        print(t, len(inventories))
-    print(inventories)
-    return bp.num * i.geodes
+        if len(inventories) > max_inventories:
+            inventories = deque(sorted(inventories)[-max_inventories:])
+        # if t >= kill_no_clay_robot_inventories_starting_at:
+        #     inventories = deque(i for i in inventories if i.clay_robots > 0)
+        # if t >= kill_no_geode_inventories_starting_at:
+        #     inventories = deque(i for i in inventories if i.geodes > 0)
+    return bp.num * max(i.geodes for i in inventories)
 
 
 def first(puzzle):
@@ -144,7 +175,9 @@ def first(puzzle):
     blueprints = get_blueprints(puzzle)
     qls = []
     for bp in blueprints:
-        qls.append(quality_level(bp))
+        ql = quality_level(bp)
+        print(ql)
+        qls.append(ql)
     return sum(qls)
 
 
@@ -159,4 +192,4 @@ def test():
 
 if __name__ == "__main__":
     print(first(PUZZLE))
-    print(second(PUZZLE))
+    # print(second(PUZZLE))
