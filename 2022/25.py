41b0204d974b7a47bda94dd7a91418b932808bdc
Leo Robinovitch <leorobinovitch@gmail.com>
2022-12-30 18:18:32 -0800

    Clean up Day 25, DONE!

--- 2022/25.py
+++ 2022/25.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from collections import deque
 
 import common
@@ -21,47 +19,35 @@ TEST_PUZZLE = """1=-0-2
 122
 """
 
+MIN_NUM_AT_PLACE = {}
+for place in range(50):
+    MIN_NUM_AT_PLACE[place] = sum(-2 * 5 ** p for p in range(place))
+MAX_NUM_AT_PLACE = {k: -v for k, v in MIN_NUM_AT_PLACE.items()}
+NUM_TO_SNAFU = {-2: "=", -1: "-", 0: "0", 1: "1", 2: "2"}
+SNAFU_TO_NUM = {v: k for k, v in NUM_TO_SNAFU.items()}
+
 
 def from_snafu(snafu):
     res = 0
     for i, v in enumerate(reversed(list(snafu))):
-        if v == "-":
-            m = -1
-        elif v == "=":
-            m = -2
-        else:
-            m = int(v)
-        res += 5 ** i * m
+        res += 5 ** i * SNAFU_TO_NUM[v]
     return res
 
 
 def to_string(nums):
     res = ""
     for n in nums:
-        if n == -2:
-            res += "="
-        elif n == -1:
-            res += "-"
-        else:
-            res += str(n)
+        res += NUM_TO_SNAFU[n]
     return res if res == "0" else res.lstrip("0")
-
-
-min_num_at_place = {}
-for place in range(50):
-    min_num_at_place[place] = sum(-2 * 5 ** p for p in range(place))
-max_num_at_place = {k: -v for k, v in min_num_at_place.items()}
 
 
 def to_snafu(dec):
     start_place = 0
     while 2 * 5 ** start_place < dec:
         start_place += 1
-    # print(dec, start_place, 2 * 5 ** start_place)
     nums = deque([(i * 5 ** start_place, [i] + [0] * start_place, start_place) for i in range(-2, 3)])
     while nums:
         val, rep, place = nums.popleft()
-        # print(val, place, dec)
         if val == dec:
             return to_string(rep)
 
@@ -69,9 +55,10 @@ def to_snafu(dec):
         if new_place < 0:
             continue
 
-        if val > dec and val + min_num_at_place[place] > dec:
+        # skip if val impossible to reach dec even with all 2s/-2s in remaining spots
+        if val > dec and val + MIN_NUM_AT_PLACE[place] > dec:
             continue
-        if val < dec and (val + max_num_at_place[place]) < dec:
+        if val < dec and val + MAX_NUM_AT_PLACE[place] < dec:
             continue
 
         for i in range(-2, 3):
@@ -80,54 +67,12 @@ def to_snafu(dec):
             new_rep[len(new_rep) - place] = i
             nums.append((new_val, new_rep, new_place))
 
-    # @dataclass
-    # class Node:
-    #     s: str
-    #     v: int
-    #     children: List[Node]
-    #
-    # root = Node("", 0, [])
-    # q = deque([root])
-    # levels = 11  # lol this is so dumb
-    # for i in range(levels):
-    #     for _ in range(len(q)):
-    #         v = q.popleft()
-    #         for child in (
-    #             Node("=", -2 * 5 ** i, []),
-    #             Node("-", -1 * 5 ** i, []),
-    #             Node("0", 0 * 5 ** i, []),
-    #             Node("1", 1 * 5 ** i, []),
-    #             Node("2", 2 * 5 ** i, []),
-    #         ):
-    #             v.children.append(child)
-    #             q.append(child)
-    #
-    # def find_sum(v, n: Node):
-    #     if not len(n.children):
-    #         if v == n.v:
-    #             return [n.s]
-    #         else:
-    #             return None
-    #     r = v - n.v
-    #     for child in n.children:
-    #         if (p := find_sum(r, child)) is not None:
-    #             return p + [n.s]
-    # res = "".join(find_sum(dec, root)).lstrip("0")
-    # if res == "":
-    #     return "0"
-    # return -1
-
 
 def first(puzzle):
     res = 0
     for sn in puzzle.split("\n")[:-1]:
         res += from_snafu(sn)
-    # print(res)
     return to_snafu(res)
-
-
-def second(puzzle):
-    return -1
 
 
 def test():
@@ -139,9 +84,8 @@ def test():
     assert to_snafu(25) == "100"
     assert to_snafu(1257) == "20012"
     assert to_snafu(353) == "1=-1="
-    # assert second(TEST_PUZZLE) == -1
+    assert first(PUZZLE) == "2011-=2=-1020-1===-1"
 
 
 if __name__ == "__main__":
     print(first(PUZZLE))
-    print(second(PUZZLE))
