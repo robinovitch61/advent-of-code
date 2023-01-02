868ee55d23dc92883132f61b917986c850a85f10
Leo Robinovitch <leorobinovitch@gmail.com>
2023-01-01 18:06:24 -0800

    Improve Day 25

--- 2022/25.py
+++ 2022/25.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 import common
 
 PUZZLE = common.string(25)
@@ -19,53 +17,38 @@ TEST_PUZZLE = """1=-0-2
 122
 """
 
-MIN_NUM_AT_PLACE = {}
-for place in range(50):
-    MIN_NUM_AT_PLACE[place] = sum(-2 * 5 ** p for p in range(place))
-MAX_NUM_AT_PLACE = {k: -v for k, v in MIN_NUM_AT_PLACE.items()}
-NUM_TO_SNAFU = {-2: "=", -1: "-", 0: "0", 1: "1", 2: "2"}
-SNAFU_TO_NUM = {v: k for k, v in NUM_TO_SNAFU.items()}
-
 
 def from_snafu(snafu):
     res = 0
     for i, v in enumerate(reversed(list(snafu))):
-        res += 5 ** i * SNAFU_TO_NUM[v]
+        if v == "=":
+            m = -2
+        elif v == "-":
+            m = -1
+        else:
+            m = int(v)
+        res += 5 ** i * m
     return res
 
 
-def to_string(nums):
-    res = ""
-    for n in nums:
-        res += NUM_TO_SNAFU[n]
-    return res if res == "0" else res.lstrip("0")
+def _to_snafu(dec):
+    if dec == 0:
+        return ""
+    remainder, quotient, carry = dec % 5, dec // 5, (dec + 5) // 5
+    if remainder == 2:
+        return _to_snafu(quotient) + "2"
+    elif remainder == 1:
+        return _to_snafu(quotient) + "1"
+    elif remainder == 0:
+        return _to_snafu(quotient) + "0"
+    elif remainder == 3:
+        return _to_snafu(carry) + "="
+    elif remainder == 4:
+        return _to_snafu(carry) + "-"
 
 
 def to_snafu(dec):
-    start_place = 0
-    while 2 * 5 ** start_place < dec:
-        start_place += 1
-    nums = deque([(i * 5 ** start_place, [i] + [0] * start_place, start_place) for i in range(-2, 3)])
-    while nums:
-        val, rep, place = nums.popleft()
-        if val == dec:
-            return to_string(rep)
-
-        new_place = place - 1
-        if new_place < 0:
-            continue
-
-        # skip if val impossible to reach dec even with all 2s/-2s in remaining spots
-        if val > dec and val + MIN_NUM_AT_PLACE[place] > dec:
-            continue
-        if val < dec and val + MAX_NUM_AT_PLACE[place] < dec:
-            continue
-
-        for i in range(-2, 3):
-            new_val = val + i * 5 ** new_place
-            new_rep = rep.copy()
-            new_rep[len(new_rep) - place] = i
-            nums.append((new_val, new_rep, new_place))
+    return _to_snafu(dec) or "0"
 
 
 def first(puzzle):
